8192ffb87f1448a53262e168aa4581b4dd3b73f2
Subhash Chandran <shankarakumar.s@northeastern.edu>
2023-12-06 09:23:53 -0500

    Email verification

--- controllers/user.js
+++ controllers/user.js
@@ -1,4 +1,6 @@
+const nodemailer = require('nodemailer')
 const User = require('../models/user')
+const EmailVerificationToken = require('../models/emailVerificationToken')
 
 exports.create = async (req, res) => {
   const { name, email, password } = req.body
@@ -10,5 +12,38 @@ exports.create = async (req, res) => {
   const newUser = new User({ name, email, password })
   await newUser.save()
 
-  res.status(201).json({ user: newUser })
+  // generate 6 digit otp
+  let OTP = "";
+  for (let i = 1; i <= 5; i++) {
+    const randomVal = Math.round(Math.random() * 9);
+    OTP += randomVal;
+  }
+
+  // store otp inside our db
+  const newEmailVerificationToken = new EmailVerificationToken({ owner: newUser._id, token: OTP })
+
+  await newEmailVerificationToken.save()
+
+  // send that otp to our user
+
+  var transport = nodemailer.createTransport({
+    host: "smtp.mailtrap.io",
+    port: 2525,
+    auth: {
+      user: "f29ed41f1f4bd3",
+      pass: "db3edd88e2927f"
+    }
+  });
+
+  transport.sendMail({
+    from: 'verification@reviewapp.com',
+    to: newUser.email,
+    subject: 'Email Verification',
+    html: `
+      <p>You verification OTP</p>
+      <h1>${OTP}</h1>
+    `
+  })
+
+  res.status(201).json({ message: 'Please verify you email. OTP has been sent to your email account!' })
 };
