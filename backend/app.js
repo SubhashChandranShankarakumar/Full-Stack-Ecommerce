8f099cd6768f2a177e95a491d1171aec38226c9b
Subhash Chandran <shankarakumar.s@northeastern.edu>
2023-12-28 04:13:53 -0500

    Reset Email

--- backend/app.js
+++ backend/app.js
@@ -1,30 +1,21 @@
 const express = require("express");
-require("express-async-errors");
-const morgan = require("morgan");
-const { errorHandler } = require("./middlewares/error");
-require("dotenv").config();
-require("./db");
+require('./db')
 const userRouter = require("./routes/user");
 
 const app = express();
-app.use(express.json());
-app.use(morgan("dev"));
+app.use(express.json())
 app.use("/api/user", userRouter);
 
-app.use(errorHandler);
-
-// app.post(
-//   "/sign-in",
-//   (req, res, next) => {
-//     const { email, password } = req.body;
-//     if (!email || !password)
-//       return res.json({ error: "email/password missing!" });
-//     next();
-//   },
-//   (req, res) => {
-//     res.send("<h1>Hello I am about page</h1>");
-//   }
-// );
+app.post("/sign-in",
+  (req, res, next) => {
+    const { email, password } = req.body
+    if (!email || !password)
+      return res.json({ error: 'email/ password missing!' })
+    next()
+  },
+  (req, res) => {
+    res.send("<h1>Hello I am from your backend about</h1>");
+  });
 
 app.listen(8000, () => {
   console.log("the port is listening on port 8000");
